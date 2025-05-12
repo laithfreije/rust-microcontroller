21706f4489115706c48a6f98a665ea3bb2fb7755
Laith Freije <laithfreije@pm.me>
2025-05-12 18:07:17 -0400

    fix: Issue #7 Use Clock Module
    - Used clock module to blink LED at set interval

--- setup-env.sh
+++ setup-env.sh
@@ -1,4 +1,2 @@
 rustup target add thumbv6m-none-eabi
-sudo apt-get install pkg-config -y
-sudo apt-get install libudev-dev -y
 cargo install elf2uf2-rs
--- src/clock.rs
+++ src/clock.rs
@@ -1,11 +1,11 @@
 // Import necessary libraries
-use rp2040_hal::clocks::{init_clocks_and_plls, ClocksManager};
-use rp2040_hal::{pac, Watchdog};
 use rp_pico::XOSC_CRYSTAL_FREQ;
+use rp2040_hal::clocks::{ClocksManager, init_clocks_and_plls};
+use rp2040_hal::{Watchdog, pac};
 
 #[allow(unused)]
 /// A struct for managing clocks in the Raspberry PI Pico H.
-pub struct ClockAPI{
+pub struct ClockAPI {
     // Define necessary clock variables here
     pub clocks: ClocksManager,
 }
@@ -18,7 +18,7 @@ impl ClockAPI {
         pll_sys: pac::PLL_SYS,
         pll_usb: pac::PLL_USB,
         mut resets: &mut pac::RESETS, // Only this needs to be a reference
-        watchdog: &mut Watchdog,  // And this
+        watchdog: &mut Watchdog,      // And this
     ) -> Self {
         let clocks = init_clocks_and_plls(
             XOSC_CRYSTAL_FREQ,
@@ -28,7 +28,9 @@ impl ClockAPI {
             pll_usb,
             &mut resets,
             watchdog,
-        ).ok().unwrap();
+        )
+        .ok()
+        .unwrap();
 
         Self { clocks }
     }
